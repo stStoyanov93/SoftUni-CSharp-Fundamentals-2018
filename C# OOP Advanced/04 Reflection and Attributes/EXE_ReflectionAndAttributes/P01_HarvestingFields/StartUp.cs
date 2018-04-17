9bb46ac106134a60aa97e08bfba1a419dbb96899
stStoyanov93 <sk.stoyanov93@gmail.com>
2018-04-17 20:30:55 +0300

    Added the first two tasks from the Reflection exercises

---
+++ C# OOP Advanced/04 Reflection and Attributes/EXE_ReflectionAndAttributes/P01_HarvestingFields/StartUp.cs
@@ -0,0 +1,57 @@
+﻿ namespace P01_HarvestingFields
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var classType = typeof(HarvestingFields);
+            var fields = classType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "HARVEST")
+            {
+                switch (input)
+                {
+                    case "private":
+                        var privateFields = fields.Where(f => f.IsPrivate);
+                        PrintFields(input, privateFields);
+                        break;
+                    case "protected":
+                        var protectedFields = fields.Where(f => f.IsFamily);
+                        PrintFields(input, protectedFields);
+                        break;
+                    case "public":
+                        var publicFields = fields.Where(f => f.IsPublic);
+                        PrintFields(input, publicFields);
+                        break;
+                    case "all":
+                        PrintAllFields(fields);
+                        break;
+                }
+            }
+
+        }
+
+        public static void PrintFields(string modifierType, IEnumerable<FieldInfo> fields)
+        {
+            foreach (var field in fields)
+            {
+                Console.WriteLine($"{modifierType} {field.FieldType.Name} {field.Name}");
+            }
+        }
+
+        public static void PrintAllFields(IEnumerable<FieldInfo> fields)
+        {
+            foreach (var field in fields)
+            {
+                var acessModifier = field.Attributes.ToString().ToLower() == "family" ? "protected" : field.Attributes.ToString().ToLower();
+                Console.WriteLine($"{acessModifier} {field.FieldType.Name} {field.Name}");
+            }
+        }
+    }
+}
---
+++ C# OOP Advanced/04 Reflection and Attributes/EXE_ReflectionAndAttributes/P02_BlackBoxInteger/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var classType = typeof(BlackBoxInteger);
+            var blackBoxInstance = Activator.CreateInstance(classType, true);
+
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var inputParams = input.Split("_");
+                var methodName = inputParams[0];
+                var value = int.Parse(inputParams[1]);
+
+                classType.GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic)
+                    .Invoke(blackBoxInstance, new object[] { value });
+
+                var innerValue = classType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                    .Where(f => f.Name == "innerValue").First().GetValue(blackBoxInstance);
+
+                Console.WriteLine(innerValue);
+            }
+        }
+    }
+}
